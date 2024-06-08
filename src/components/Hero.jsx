ec1d1a459eac644087afec3b6ac0ce22d54d0bf8
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2024-06-08 13:45:30 +0200

    implemented-first-3d-model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
+    <section className=' relative w-full 
+    h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120] max-w-7xl mx-auto flex 
+      flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-40'>
+          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+        </div>
+
+        <div className='mt-36'>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>
+            Rourke</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            An aspiring Full Stack Developer, <br className='sm:block hidden' />
+            with a passion to create beautiful web applications . . .
+          </p>
+        </div>
+      </div>
+      
+      <ComputersCanvas />
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <nav
       className={`${styles.paddingX} w-full flex 
-      items-center py-5 fixed top0 z-20 bg primary`}
+      items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className='w-full flex justify-between 
       items-center max-w-7xl mx-auto'
