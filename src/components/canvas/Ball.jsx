5f9ec40c2ff8d44ea21e3b423859c412a444428c
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2024-06-09 18:34:56 +0200

    implemented-floating-balls-section
    Float from @react-three/drei is not rendering/functioning properly

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { Suspense, useMemo } from 'react';
+import { Canvas } from '@react-three/fiber';
+import {
+  Decal, Float, OrbitControls, Preload, useTexture, Html
+} from '@react-three/drei';
 
-const Ball = () => {
+import CanvasLoader from '../Loader';
+
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
+
   return (
-    <div>Ball</div>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color='#ff8eb'
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          flatShading
+          scale={1}
+          map={decal}
+        />
+      </mesh>
+    </Float>
   )
 }
 
-export default Ball
+const BallCanvas = ({ icon }) => {
+  const memoizedCanvas = useMemo(() => (
+    // return (
+    <Canvas
+      frameloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<Html><CanvasLoader /></Html>}>
+        <OrbitControls
+          enableZoom={false}
+        />
+        <Ball imgUrl={icon} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+    // )
+  ), [icon]);
+  return memoizedCanvas;
+}
+
+export default BallCanvas;
